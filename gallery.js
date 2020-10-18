98b95e1c136d6f8365d288f4cc90efd0b2a08243
Lyudmyla Boiko <itsforyou.com.ua@gmail.com>
2020-10-18 14:00:22 +0300

    I'm ready

--- gallery.js
+++ gallery.js
@@ -6,8 +6,14 @@ console.log(createImagesCardsMarkup(images));
 const galleryContainer = document.querySelector('.js-gallery');
 const imagesMarkup = createImagesCardsMarkup(images);
 
+const openModal = document.querySelector('div.lightbox');
+const openImageInModal = document.querySelector('.lightbox__image');
+
+const buttonCloseModal = document.querySelector('.lightbox__button');
+
 galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
 galleryContainer.addEventListener('click', onGalleryContainerClick);
+buttonCloseModal.addEventListener('click', buttonCloseModalClick);
 
 function createImagesCardsMarkup(images) {
   return images.map(({preview, original, description}) => {
@@ -31,8 +37,34 @@ function createImagesCardsMarkup(images) {
 }
   
 function onGalleryContainerClick(event) { 
-  if (!event.target.classList.contains('gallery__image')) {
+  event.preventDefault();
+  const isGalleryEl = event.target.classList.contains('js-gallery');
+  if (!!isGalleryEl) {
     return;
   }
-  console.log(event.target.dataset.original);
+  
+  console.log(event.target.dataset.source);
+  
+
+  onOpenModalClick();
+  onAddModalImage(event);
+};
+
+function onOpenModalClick() {
+  openModal.classList.add('is-open');
+};
+
+function onAddModalImage(event) {
+  openImageInModal.src = event.target.dataset.source;
+  openImageInModal.alt = event.target.alt;
+};
+ 
+function buttonCloseModalClick() {
+  openModal.classList.remove('is-open');
+  onClearModalImage()
+};
+
+ function onClearModalImage() {
+  openImageInModal.src = ``;
+  openImageInModal.alt = ``;
 };
